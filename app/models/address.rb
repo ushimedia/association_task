dc9c928ada3cb8dc03fcfc1c6e7d7c60df491eeb
ushioda <yusuke_ushioda@mediado.jp>
2022-04-18 17:14:57 +0900

    課題

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,4 +2,7 @@ class Address < ApplicationRecord
   has_many :orders
   belongs_to :customer
   belongs_to :shop
+  has_many :address_num1, class_name: "Address", foreign_key: "addressable_id"
+  belongs_to :address_num2, class_name: "Address", optional: true
+
 end
